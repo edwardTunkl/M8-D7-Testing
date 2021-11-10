084dab353b96fd722b11f4be596a19a037059980
edwardTunkl <eddytunkl@googlemail.com>
2021-11-10 11:08:05 +0100

    updated tests

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import { app } from './app.js';
 import mongoose from 'mongoose';
 import list from "express-list-endpoints"
+import listEndpoints from "express-list-endpoints";
+
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("Connected to Mongo locally")
     app.listen(process.env.PORT, () => {
+        console.table(listEndpoints(app))
         console.log("Server is running on port " + process.env.PORT)
         console.log(list(app))
     })
--- src/services/products/index.js
+++ src/services/products/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import createHttpError from "http-errors";
 import { ProductModel } from "./model.js";
 
 const productsRouter = express.Router();
@@ -25,5 +26,51 @@ productsRouter.post("/", async (req, res) => {
 
     res.status(201).send({ product });
 })
+
+productsRouter.get("/:id", async (req, res, next) => {
+    try {
+      const products = await ProductModel.findById(req.params.id);
+      if (products) {
+        res.send(products);
+      } else {
+        next(createHttpError(404, "Not found"));
+      }
+    } catch (error) {
+      next(createHttpError(500));
+    }
+  });
+  //
+  productsRouter.delete("/:id", async (req, res, next) => {
+    try {
+      const deletedProduct = await ProductModel.findByIdAndDelete(req.params.id);
+      if (deletedProduct) {
+        res.status(204).send();
+      } else {
+        next(createHttpError(404, "Not found"));
+      }
+    } catch (error) {
+      next(createHttpError(500));
+    }
+  });
+  //
+  productsRouter.put("/:id", async (req, res, next) => {
+    try {
+      const { name, price } = req.body;
+      if (!name || !price) {
+        res.status(400).send({ error: "name and price are required" });
+        return;
+      }
+      const product = await ProductModel.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {
+          new: true,
+        }
+      );
+      res.status(201).send(product);
+    } catch (error) {
+      next(createHttpError(500));
+    }
+  });
 
 export { productsRouter }
